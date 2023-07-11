a26c3bcc50e3a26856eb4caef7eb5117d07b86f7
Juan Manuel Rodriguez Perez <jmrp81@gmail.com>
2023-07-11 18:17:58 +0200

    create container and its styles

--- CSS framework/src/index.ts
+++ CSS framework/src/index.ts
@@ -11,16 +11,40 @@ declare global {
 
 // #region general functions
 function showHideTab(tabId: string): void {
-    const tableWithGraphElement = document.getElementById('tabBarGraph');
+    const tabBarGraph = document.getElementById('tabBarGraph');
+    const tabLineGraph = document.getElementById('tabLineGraph');
 
-    if (tableWithGraphElement) {
-        tableWithGraphElement.style.display = "none";
+    if (tabBarGraph) {
+        tabBarGraph.style.display = "none";
+    }
+
+    if (tabLineGraph) {
+        tabLineGraph.style.display = "none";
     }
 
     const currentTab = document.getElementById(tabId);
 
     if (currentTab) {
         currentTab.style.display = "flex";
+    }
+
+
+    const button_tabBarGraph = document.getElementById('button_tabBarGraph');
+    const button_tabLineGraph = document.getElementById('button_tabLineGraph');
+
+    if (button_tabBarGraph) {
+        button_tabBarGraph.classList.remove('buttonSelected');
+    }
+
+    if (button_tabLineGraph) {
+        button_tabLineGraph.classList.remove('buttonSelected');
+    }
+
+    const currentButtonId = 'button_' + tabId;
+    const currentButton = document.getElementById(currentButtonId);
+
+    if (currentButton) {
+        currentButton.classList.add('buttonSelected');
     }
 
     drawReeData();
